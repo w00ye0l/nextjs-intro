6e9b14d798184f2db8cb5d06094c615af8e61023
wooyeol <dnduf158@gmail.com>
2024-01-17 19:48:11 +0900

    add: navigation bar & use router hook

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from "@/components/NavBar";
+
 export default function Tomato() {
-  return "about test";
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import NavBar from "@/components/NavBar";
+
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <h1>Hello</h1>
     </div>
   );
