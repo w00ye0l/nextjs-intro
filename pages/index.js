3369ab97e0054cabfbce6faaa6ede9239f5241cd
wooyeol <dnduf158@gmail.com>
2024-01-18 20:04:40 +0900

    fix: movie detail page title and id to ssr

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,7 @@ import { useRouter } from "next/router";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title: title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -27,13 +19,7 @@ export default function Home({ results }) {
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
             <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}
+              href={`/movies/${movie.original_title}/${movie.id}`}
               legacyBehavior
             >
               <a>{movie.original_title}</a>
