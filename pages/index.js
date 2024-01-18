b7fd9bc5a0eede27159b6f457554b90e96ecdc9b
wooyeol <dnduf158@gmail.com>
2024-01-18 17:16:18 +0900

    add: data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "afc82667845b09c1c37bd166abd0c812";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
